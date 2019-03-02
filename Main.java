ae6e76a492ee0ac88cf49c16ade80dfb3a42dd59
annalapuc <43343474+annalapuc@users.noreply.github.com>
2019-03-02 23:33:43 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         String SecAndMin = SecondAndMinutes.getDurationString(61, 0);
         System.out.println(SecAndMin);
+
         SecAndMin = SecondAndMinutes.getDurationString(3669);
         System.out.println(SecAndMin);
+
+        SecAndMin = SecondAndMinutes.getDurationString(-49);
+        System.out.println(SecAndMin);
     }
-
-
 }
