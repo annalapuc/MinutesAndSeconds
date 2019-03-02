74006f66b92e4b0bb9f44d8f3aa0c8229026b3a2
annalapuc <43343474+annalapuc@users.noreply.github.com>
2019-03-02 23:34:19 +0100

    Update SecondAndMinutes.java

--- SecondAndMinutes.java
+++ SecondAndMinutes.java
@@ -1,23 +1,46 @@
 public class SecondAndMinutes {
-    public static String getDurationString(int minutes, int seconds){
-        if(minutes>=0&&seconds>=0&&seconds<=59){
-           int hours=(int)minutes/60;
-           int remainingMinutes=(int)minutes%60;
 
+    private static final String INVALID_VALUE_MESSAGE = "Invalid value";
 
-            return minutes+" m = "+hours+"h "+remainingMinutes+"m "+seconds+"s";
-        }else{
+    public static String getDurationString(int minutes, int seconds) {
 
-            return "Invalid value";
+        if (minutes >= 0 && seconds >= 0 && seconds <= 59) {
+            int hours = (int) minutes / 60;
+            int remainingMinutes = (int) minutes % 60;
+
+            String hoursString = hours + "h";
+            if (hours < 10) {
+                hoursString = "0" + hoursString;
+            }
+
+            String minutesString = remainingMinutes + "h";
+            if (remainingMinutes < 10) {
+                minutesString = "0" + minutesString;
+            }
+
+            String secondsString = seconds + "h";
+            if (seconds < 10) {
+                secondsString = "0" + secondsString;
+            }
+
+            return minutes + " m = " + hoursString + " " + minutesString + " " + secondsString + "";
+
+        } else {
+
+            return INVALID_VALUE_MESSAGE;
         }
     }
-    public static String getDurationString(int seconds){
-        if(seconds>=0){
-            int minutes=(int)seconds/60;
-            int remainingSeconds=(int)seconds%60;
-            return getDurationString(minutes,remainingSeconds);
+
+    public static String getDurationString(int seconds) {
+
+        if (seconds >= 0) {
+            int minutes = (int) seconds / 60;
+            int remainingSeconds = (int) seconds % 60;
+
+            return getDurationString(minutes, remainingSeconds);
+
         } else {
-            return "Invalid value";
+            return INVALID_VALUE_MESSAGE;
         }
     }
 }
